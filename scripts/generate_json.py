18a19251bd139cc35d1999b0e72a8f0481192e95
aereswalls <leonardomazzone94@gmail.com>
2025-05-22 17:37:43 +0200

    Update generate_json.py

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -3,7 +3,7 @@ import json
 import re
 from datetime import datetime
 
-CATEGORIES = ["astro", "nature", "colors", "stock", "abstract", "architecture", "texture"]
+CATEGORIES = ["astro", "nature", "colors", "stock", "abstract", "architecture", "texture", "dark"]
 BASE_URL = "https://raw.githubusercontent.com/aereswalls/wallpaper-json/main/"
 
 def clean_id(filename):
